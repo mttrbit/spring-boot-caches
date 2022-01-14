1d3fb21129881854ae36530bb35f2cb195868c26
Sebastian Kaiser <sk4is3r@gmail.com>
2022-01-13 20:27:10 -0800

    Init  commit

--- spring-caches-metrics-autoconfigure/README.md
+++
@@ -1,1 +0,0 @@
-# Spring Caches Metrics Autoconfigure
--- spring-caches-platform/src/main/java/spring/caches/backend/properties/tree/NodeHandler.java
+++
@@ -1,9 +0,0 @@
-package spring.caches.backend.properties.tree;
-
-/**
- * tbd.
- * @param <C> tbd.
- */
-public interface NodeHandler<C> {
-    void handle(Node node, C context);
-}
